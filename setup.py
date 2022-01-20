6b6bcda31cbad6bf5a0bd7f40fe4d84bd09443bf
baurls <33152561+baurls@users.noreply.github.com>
2022-01-20 03:06:28 +0100

    [🐞] Bugfix: Pillow Package

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 setup(
     name='map_tile_stitcher',
     packages=['map_tile_stitcher'],
-    version='0.3',
+    version='0.4',
     license='apache-2.0',
     description='Downloads and stitches tiles to a large high-resolution map on any zoom level',
     author='Lukas Baur',
     url='https://github.com/baurls/TileStitcher',  # Provide either the link to your github or to your website
-    download_url='https://github.com/baurls/TileStitcher/archive/refs/tags/v0.3.tar.gz',
+    download_url='https://github.com/baurls/TileStitcher/archive/refs/tags/v0.4.tar.gz',
     keywords=['tiling', 'map', 'tiles'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'joblib',
         'tqdm',
-        'PIL'
+        'Pillow'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
